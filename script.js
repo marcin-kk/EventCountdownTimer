14188864de1e2a5dfca0faf3c2ddee414543e5a4
Marcin <mkula.poczta@gmail.com>
2023-07-04 11:35:01 +0200

    JS: event name now populating in the app body and changing picture functionality added

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('hello');
+const settingsBtn = document.querySelector(".settings-btn")
+const settingsPanel = document.querySelector(".settings")
+const saveBtn = document.querySelector(".save")
+const eventNameInput = document.querySelector("#event-name")
+
+const imgLinkInput = document.querySelector("#event-image")
+const imgSection = document.querySelector(".image-section")
+
+const eventNameSpan = document.querySelector("span.event")
+
+const changeImg = () => {
+	if (imgLinkInput.value === "") {
+		imgSection.style.backgroundImage = "url('default.jpg')"
+	} else {
+		const newUrl = imgLinkInput.value
+		imgSection.style.backgroundImage = `url("${newUrl}")`
+	}
+}
+
+const showEventName = () => {
+	eventNameSpan.textContent = eventNameInput.value
+}
+
+settingsBtn.addEventListener("click", () => {
+	settingsPanel.classList.toggle("active")
+})
+
+saveBtn.addEventListener("click", showEventName)
+saveBtn.addEventListener("click", changeImg)
